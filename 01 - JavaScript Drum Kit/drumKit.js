cd2d10a70cc8973e8279a3bd1adccdc622c27a04
SofijaSutton <srlsutton@gmail.com>
2021-04-12 16:17:49 -0400

    Complete drumKit sounds

--- 01 - JavaScript Drum Kit/drumKit.js
+++ 01 - JavaScript Drum Kit/drumKit.js
@@ -1,9 +1,18 @@
-document.addEventListener('keydown', (e) => {
-  const drumKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!drumKey) {
-    return;
-  }
-  sound.currentTime = 0;
-  sound.play();
-});
+function removeTransition(e) {
+	if (e.propertyName !== 'transform')	return;
+	e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+	const drumKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	if (!sound)	return;
+	drumKey.classList.add('playing');
+	sound.currentTime = 0;
+	sound.play();
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown', playSound);
