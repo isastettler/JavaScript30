c6a749f50e354a1442002664616d11c63c8f4dc9
Isabelle Stettler <aysa.stettler@gmail.com>
2021-04-19 20:40:53 -0400

    remove red borders

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -6,5 +6,4 @@ allPanel.forEach(panel => panel.addEventListener("click", onClick))
 function onClick(e){
     allPanel.forEach(panel => panel.classList.remove("open"))
    e.target.classList.add("open")
-   console.log(e.target.children)
 }
