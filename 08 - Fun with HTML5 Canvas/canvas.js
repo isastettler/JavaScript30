b688e2a331fb48691a9522a25b0b989517dcea9b
Olivia Wong <oliviawongnyc@gmail.com>
2021-04-29 17:42:29 -0400

    Finished 08

---
+++ 08 - Fun with HTML5 Canvas/canvas.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById('draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// ctx.fillStyle = 'green';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let lineWidth = 1;
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  ctx.lineWidth = `${lineWidth}`;
+  lineWidth = Math.random() * 100;
+});
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
+canvas.addEventListener('mousemove', draw);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  // console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+}
+
+// ctx.lineWidth = 10;
+// ctx.fillRect(0, 0, canvas.width, canvas.height);
+// ctx.fill();
